e7ab4733035281505bbd59d94e24c92bea22c9cd
Tarandeep Singh <55691535+singhstack@users.noreply.github.com>
2020-01-21 12:28:18 +0530

    Initialize locations for original/train/val/test datasets

---
+++ cancernet/config.py
@@ -0,0 +1,9 @@
+import os
+INPUT_DATASET = "datasets\original"
+BASE_PATH = "datasets\idc"
+TRAIN_PATH = os.path.sep.join([BASE_PATH, "training"])
+print(TRAIN_PATH)
+VAL_PATH = os.path.sep.join([BASE_PATH, "validation"])
+TEST_PATH = os.path.sep.join([BASE_PATH, "testing"])
+TRAIN_SPLIT = 0.8
+VAL_SPLIT = 0.1
